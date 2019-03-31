d87fcdb7e56e9635ac7dc2883e22fab8dfe10090
Blend Mexhuani <blendmexhuani@hotmail.com>
2019-03-31 01:48:31 +0100

    Added code for product image upload

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -4,7 +4,7 @@ import { withRouter } from "react-router-dom";
 import { graphql } from 'react-apollo';
 import { gql } from 'apollo-boost';
 import * as moment from 'moment';
-
+import axios from 'axios';
 
 function success() {
   Modal.success({
@@ -12,7 +12,6 @@ function success() {
     content: 'Press OK to continue...',
   });
 }
-
 
 class AddProduct extends Component {
     state = {
@@ -24,11 +23,12 @@ class AddProduct extends Component {
         taxRate: '',
         salePrice: '',
         specialDiscount: '',
-        addError: false
+        addError: false,
+        selectedFile: ''
       }
     
     handleSubmit = (data, sPrice) => {
-        const {name, description, quantity, imageUrl, basePrice, taxRate, specialDiscount} = data;
+        const {name, description, quantity, imageUrl, basePrice, taxRate, specialDiscount, selectedFile} = data;
 
         if(name == "" || description == "" || quantity == "" || imageUrl == "" || 
           basePrice == ""){
@@ -36,61 +36,74 @@ class AddProduct extends Component {
         }
 
         else{
-          this.props.addProductMutation({
-            variables: {
-                name, 
-                description, 
-                imageUrl: imageUrl.replace(/^.*[\\\/]/, ''),
-                quantity: parseInt(quantity, 10), 
-                basePrice: parseInt(basePrice,10), 
-                taxRate: isNaN(parseInt(taxRate,10)) ? 0 : parseInt(taxRate,10),
-                specialDiscount: isNaN(parseInt(specialDiscount,10)) ? 0 : parseInt(specialDiscount,10), 
+          let formData = new FormData();
+          formData.append('myImage', selectedFile);
+          axios.post('http://localhost:5000/upload', formData)
+            .then(res => {
+              return res.data.split("/")[1];
+            })
+            .then(res => {
+              this.props.addProductMutation({
+              variables: {
+                name,
+                description,
+                imageUrl: res,
+                quantity: parseInt(quantity, 10),
+                basePrice: parseInt(basePrice, 10),
+                taxRate: isNaN(parseInt(taxRate, 10)) ? 0 : parseInt(taxRate, 10),
+                specialDiscount: isNaN(parseInt(specialDiscount, 10)) ? 0 : parseInt(specialDiscount, 10),
                 salePrice: sPrice,
                 timestamps: moment().format(),
                 id: this.props.userID
-            },
-            refetchQueries: [
-              {
-                query: gql`
-                query products{
-                  products{
-                      id
-                      name
-                      description
-                      imageUrl
-                      quantity
-                      basePrice
-                      taxRate
-                      salePrice
-                      specialDiscount
-                      creator {
-                          id
-                          username
+              },
+              refetchQueries: [
+                {
+                  query: gql`
+                      query products{
+                        products{
+                            id
+                            name
+                            description
+                            imageUrl
+                            quantity
+                            basePrice
+                            taxRate
+                            salePrice
+                            specialDiscount
+                            creator {
+                                id
+                                username
+                            }
+                            isDeleted
+                        }
                       }
-                      isDeleted
+                    `
                   }
-                }
-              `
-              }
-            ]
-            }).then(result => {
-              this.setState({ 
-                name: "",
-                description: "",
-                imageUrl: "",
-                quantity: '',
-                basePrice: '',
-                taxRate: '',
-                salePrice: '',
-                specialDiscount: '',
-                id: '',
-                addError: false });
+                ]
+              }).then(result => {
+                this.setState({
+                  name: "",
+                  description: "",
+                  imageUrl: "",
+                  quantity: '',
+                  basePrice: '',
+                  taxRate: '',
+                  salePrice: '',
+                  specialDiscount: '',
+                  selectedFile: '',
+                  id: '',
+                  addError: false
+                });
                 success();
+              })
+              .catch(err => {
+                this.setState({ addError: true });
+                console.log("Error while adding the product: ", err);
+              })
             })
-            .catch(err => {
-              this.setState({addError: true});
-              console.log("Error while adding the product: ", err);
-            })
+            .catch((err) => {
+              console.log(err);
+            });
           }
         }
     
@@ -104,8 +117,16 @@ class AddProduct extends Component {
             taxRate: '',
             salePrice: '',
             specialDiscount: '',
+            selectedFile: '',
             addError: false });
       }
+
+    imageChange = (event) => {
+        this.setState({ 
+            imageUrl: event.target.value,
+            selectedFile: event.target.files[0]
+        });
+    }
 
     render() {
 
@@ -114,7 +135,7 @@ class AddProduct extends Component {
         var sPrice = (this.state.basePrice + tax) - ((this.state.basePrice + tax) * (this.state.specialDiscount/100));
         
         return (
-            <Form className="login-form addProduct">
+            <Form className="login-form addProduct" enctype="multipart/form-data">
                     <Form.Item>Name: <span style={{color: "#ff0000"}}>*</span>
                         <Input value={this.state.name} onChange={(event)=>{
                           this.setState({name: event.target.value}); }}/>
@@ -123,9 +144,9 @@ class AddProduct extends Component {
                         <Input value={this.state.description} onChange={(event)=>{
                           this.setState({description: event.target.value}); }} />
                     </Form.Item>
-                    <Form.Item>Image Url: <span style={{color: "#ff0000"}}>*</span>
-                        <input type="file" value={this.state.imageUrl} onChange={(event)=>{
-                            this.setState({imageUrl: event.target.value}); }} />
+                    <Form.Item>Image Url: <span style={{ color: "#ff0000" }}>*</span>
+                      <input type="file" name="myImage" value={this.state.imageUrl} 
+                        onChange={(e) => this.imageChange(e)} />
                     </Form.Item>
                     <Form.Item>Quantity: <span style={{color: "#ff0000"}}>*</span>
                         <Input value={this.state.quantity} onChange={(event)=>{
